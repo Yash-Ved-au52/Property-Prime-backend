7bdb1ea8009c5190d657d68dbe1a2da96690ded4
Yash ved <yashved01@gmail.com>
2024-05-04 14:56:39 +0530

    new

---
+++ api/index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors'; // Import cors package
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+// Use cors middleware
+app.use(cors({
+  origin: 'http://localhost:3001', // Allow requests from this origin
+  credentials: true // Include credentials (cookies) in cross-origin requests
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+
+// app.use(express.static(path.join(__dirname, '/app/build')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'app', 'build', 'index.html'));
+// })
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
